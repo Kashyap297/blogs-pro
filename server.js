fd60cf8b70c9086ba917346264739f0230840f3e
Kashyap297 <kashyap29700@gmail.com>
2024-03-21 23:47:25 +0530

    register, login, logout done using cooki-parser

--- server.js
+++ server.js
@@ -1,27 +1,85 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 const port = 9000;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.set('view engine', 'ejs')
+
 
 const mongoose = require('mongoose')
 const { userModel } = require('./schemas/userschema.js')
 
-app.use(express.static('public'));
+var cookieParser = require('cookie-parser')
+app.use(cookieParser());
 
-app.set('view engine', 'ejs')
 
+// authentication
+const authLogin = (req, res, next) => {
+    if (req.cookies.loginUsers) {
+        next()
+    } else {
+        console.log("failure user");
+        res.redirect('/login');
+    }
+}
+
+// home
 app.get('/', (req, res) => {
     res.render('./Pages/index')
 })
-app.get('/blogs', (req, res) => {
+
+// Blog
+app.get('/blogs', authLogin, (req, res) => {
     res.render('./Pages/blog')
 })
-app.get('/login', (req, res) => {
-    res.render('./Pages/login')
-})
+
+// register
 app.get('/register', (req, res) => {
     res.render('./Pages/register')
 })
+app.post('/register', async (req, res) => {
+    const users = req.body;
+    try {
+        const register = new userModel(users);
+        await register.save();
 
+        res.redirect('/login')
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+// login
+app.get('/login', (req, res) => {
+    res.render('./Pages/login')
+})
+app.post('/login', async (req, res) => {
+    let loginUser = req.body;
+    const user = await userModel.findOne({ email: loginUser.email });
+    if (user) {
+        if (user.password === loginUser.password) {
+            res.cookie('loginUsers', user, { maxAge: 900000 })
+            console.log('success');
+            res.redirect('/blogs')
+        } else {
+            console.log('fail');
+            res.redirect('/login')
+        }
+    }
+})
+
+
+// logout
+app.get('/logout', (req, res) => {
+    if (req.cookies.loginUsers) {
+        res.clearCookie("loginUsers");
+    }
+    res.redirect('/')
+})
+
+// server
 app.listen(port, () => {
     console.log(`server Start at http://localhost:${port}`);
 })
